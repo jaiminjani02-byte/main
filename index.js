1b9c5e5954935cc63e568798f71fa393ae0f5dd9
jaiminjani02-byte <jaiminjani02@gmail.com>
2026-05-04 14:44:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('http').createServer((req, res) => {
+  res.end('Herobrine is watching...')
+}).listen(process.env.PORT || 3000)
+
 const { createHerobrine } = require('./core/bot')
 
 createHerobrine()
