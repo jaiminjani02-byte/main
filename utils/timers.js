6d96e057df46e0fe4aff53867ef3b5b1c3ac4888
jaiminjani02-byte <jaiminjani02@gmail.com>
2026-05-04 15:06:09 +0530

    Update timers.js

--- utils/timers.js
+++ utils/timers.js
@@ -1,33 +1,5 @@
-// delay using async/await
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise(r => setTimeout(r, ms))
 }
 
-// safe interval (prevents stacking)
-function createSafeInterval(fn, time) {
-  let running = false
-
-  return setInterval(async () => {
-    if (running) return
-    running = true
-
-    try {
-      await fn()
-    } catch (err) {
-      console.log('Interval error:', err)
-    }
-
-    running = false
-  }, time)
-}
-
-// random number in range
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-module.exports = {
-  delay,
-  createSafeInterval,
-  random
-}
+module.exports = { delay }
