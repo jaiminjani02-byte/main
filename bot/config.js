1b847b24f5c551e310d10400fc5b8c9625accb3a
jaiminjani02-byte <jaiminjani02@gmail.com>
2026-05-04 14:35:15 +0530

    Delete bot directory

--- bot/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: 'darkempiresmpserver.dpdns.org',
-  port: 25797,
-  username: 'Herobrine',
-  auth: 'offline',
-  version: false // safer than forcing 1.21.11
-}
--- bot/index.js
+++
@@ -1,3 +0,0 @@
-const { createHerobrine } = require('./core/bot')
-
-createHerobrine()
