90fadfcdb2241db060b60d7d9d52709f79b47d02
jaiminjani02-byte <jaiminjani02@gmail.com>
2026-05-08 18:39:21 +0530

    Update port number in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: 'stableuniverseempire.aternos.me',
-  port: 25797,
+  port: 49705,
   username: 'Herobrine',
   auth: 'offline',
   version: 1.21.11 // safer than forcing 1.21.11
